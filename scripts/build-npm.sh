cad2192ffb5f4ba821f4cf6b8e0f73a5c19d39c8
teamktown <2530804+teamktown@users.noreply.github.com>
2025-05-06 22:28:53 -0400

    refactor: put packages into build/npm folder

--- scripts/build-npm.sh
+++ scripts/build-npm.sh
@@ -8,7 +8,12 @@ cd src/mcp-core
 npm install
 npm run sanity
 npm pack
-echo "@letsfederate/mcp-core packaged successfully."
+# make the ../build/npm directory if it does not exist
+mkdir -p ../build/npm
+# move the package to ../build/npm
+mv *.tgz ../build/npm
+
+echo "@letsfederate/mcp-core packaged successfully into ../build/npm."
 cd ../../ # Return to the root directory
 
 echo "" # Add a newline for better readability
@@ -19,9 +24,16 @@ cd src/fedmgr
 npm install
 npm run sanity
 npm pack
+# move the package to ../build/npm
+mv *.tgz ../build/npm
+
 echo "@letsfederate/fedmgr packaged successfully."
 cd ../../ # Return to the root directory
 
 echo "" # Add a newline for better readability
-echo "NPM packages built successfully and are in ./src/mcp-core and ./src/fedmgr respectively"
+echo "NPM packages built successfully and are in ../build/npm"
 echo "you will need to rebuild the corresponding docker images to use the new packages"
+echo "to install:"
+echo "npm install ./build/npm/letsfederate-mcp-core-1.2.3.tgz"
+echo "npm install ./build/npm/letsfederate-mcp-fedmgr-1.2.3.tgz"
+
